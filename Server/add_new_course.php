a15be3afe75fe36e5738f6619cb2e15bce6607c6
achiever202 <es12b1001@iith.ac.in>
2015-03-25 19:09:21 +0530

    Script to add new course.

---
+++ Server/add_new_course.php
@@ -0,0 +1,48 @@
+<?php
+	
+	/* initializing database variables. */
+	$server = 'localhost';
+	$database = 'iithcourses';
+	$username = 'root';
+	$password = '123';
+
+	/* connecting to the databse. */
+	$connection = mysqli_connect($server, $username, $password, $database);
+
+	/* checking for successful connection. */
+   	if(!$connection)
+   		die("Connection failed: " . mysqli_connect_error());
+
+   	/* for post requests. */
+   	if($_POST)
+   	{
+   		$course_id = mysqli_real_escape_string($connection, $_POST['Course_ID']);
+   		$course_title = mysqli_real_escape_string($connection, $_POST['Course_Title']);
+   		$credits = mysqli_real_escape_string($connection, $_POST['Credits']);
+   		$department = mysqli_real_escape_string($connection, $_POST['Department']);
+
+   		/* getting the record from the databse. */
+      	$sql_query = "SELECT * FROM `Courses` WHERE `Course_ID`='$course_id' LIMIT 1";
+      	$result = mysqli_query($connection, $sql_query);
+
+      	/* error in connection. */
+      	if(!$result)
+      		die("Error adding course: " . mysqli_error($connection));
+
+      	/* if no record found. */
+      	if(mysqli_num_rows($result)>0)
+      		die("Error adding course: Course already exists.");
+
+      	/* adding the new course. */
+	      $sql_query = "INSERT INTO Courses (Course_ID, Course_Title, Credits, Department_Short_Name)
+	                     VALUES ('$course_id', '$course_title', '$credits', '$department')";
+
+	      if(mysqli_query($connection, $sql_query))
+	         echo "Course added successfully!";
+	      else
+	         die("Error adding course: " . mysqli_error($connection));
+   	}
+
+   	mysqli_close($connection);
+
+?>
